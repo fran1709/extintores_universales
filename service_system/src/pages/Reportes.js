93d43d954c9ac54dc37b9f01e93fb125e25fac96
Josué Quesada <josuequesadaxd@gmail.com>
2023-01-21 23:00:56 -0600

    updated activos, reportes

--- service_system/src/pages/Reportes.js
+++ service_system/src/pages/Reportes.js
@@ -1,37 +1,39 @@
-export default function Reportes(){
-    return (
-        <body>
-            <h1>Reportes</h1>
-            <form alint-items="center">
-                <table margin="0" border="1px solid black">
-                    <tr>
-                        <th >#</th>
-                        <th>Fecha</th>
-                        <th>Servicio</th>
-                        <th>Cliente</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>03/01/2023</td>
-                        <td>Mantenimiento</td>
-                        <td>Josué Quesada</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>20/12/2022</td>
-                        <td>Venta</td>
-                        <td>Francisco Ovares</td>
-                    </tr>
-                    <tr>
-                        <td>...</td>
-                        <td>...</td>
-                        <td>...</td>
-                        <td>...</td>
-                    </tr>
-                </table>
-                <br></br>
-                <button>Enviar Reporte</button>
-            </form>
-        </body>
-    );
+import "./pageStyles.css";
+
+export default function Reportes() {
+  return (
+    <body>
+      <h1>Reportes</h1>
+      <form>
+        <table class="center" margin="0" border="1px solid black">
+          <tr>
+            <th>#</th>
+            <th>Fecha</th>
+            <th>Servicio</th>
+            <th>Cliente</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>03/01/2023</td>
+            <td>Mantenimiento</td>
+            <td>Josué Quesada</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>20/12/2022</td>
+            <td>Venta</td>
+            <td>Francisco Ovares</td>
+          </tr>
+          <tr>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+          </tr>
+        </table>
+        <br></br>
+        <button>Enviar Reporte</button>
+      </form>
+    </body>
+  );
 }
