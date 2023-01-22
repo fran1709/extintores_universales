cb4bd173d98534538898b7b12a7b65c63243e7e5
fran_ovares <fran.jav.1709@gmail.com>
2023-01-21 21:34:59 -0600

    up

--- service_system/src/pages/Factura.js
+++ service_system/src/pages/Factura.js
@@ -3,24 +3,28 @@ export default function Factura(){
         <body>
             <h1>Facturando Servicio</h1>
             <form>
-                <h3>Fecha de Factura </h3>
+                <br></br>
+                <h3>Fecha de Factura </h3><br></br>
                     <input type="date"></input>
-                <h3>Cédula</h3>
+                <br></br><br></br>
+                <h3>Cédula</h3><br></br>
                     <input type="id" placeholder="Ingrese cédula cliente"></input>
-                <h3>Servicio Brindado</h3>
+                    <br></br><br></br>
+                <h3>Servicio Brindado</h3><br></br>
                     <select>
                         <option>Mantenimiento</option>
                         <option>Recarga</option>
                         <option>Venta</option>
                         <option>Capacitación</option>
-                    </select>
-                <h3>Tipo de Factura</h3>
+                    </select><br></br><br></br>
+                <h3>Tipo de Factura</h3><br></br>
                     <select>
                         <option>Contado</option>
                         <option>Crédito</option>
                     </select>
                     <br></br><br></br>
                     <label >Factura Electrónica <input type="checkbox"></input></label>
+                    <br></br><br></br>
                 <h3>Nota</h3>
                     <textarea width="300px" height="150px" placeholder="Ingrese anotaciones a tener en cuenta"></textarea>
                 <br></br><br></br>
