711470d0507ddb7fe9ed11a1743d61122470f2e0
Josué Quesada <josuequesadaxd@gmail.com>
2023-01-21 23:35:29 -0600

    updated finanzas, rutas

--- service_system/src/pages/Finanzas.js
+++ service_system/src/pages/Finanzas.js
@@ -1,3 +1,55 @@
-export default function Finanzas(){
-    return <h1>Finanzas</h1>
+import "./pageStyles.css";
+
+export default function Finanzas() {
+  return (
+    <div>
+      <h1>Finanzas Generales E.U.</h1>
+      <form>
+        <table class="center" margin="0" border="1px solid black">
+          <tr>
+            <th>Sede</th>
+            <th>Cantidad Empleados</th>
+            <th>Rutas a Cargo</th>
+            <th>Ingresos del Mes</th>
+            <th>Gastos del Mes</th>
+            <th>Ingresos Netos</th>
+          </tr>
+          <tr>
+            <td>San Carlos</td>
+            <td>15</td>
+            <td>7</td>
+            <td>1500000</td>
+            <td>250000</td>
+            <td>1250000</td>
+          </tr>
+          <tr>
+            <td>San José</td>
+            <td>25</td>
+            <td>10</td>
+            <td>3400000</td>
+            <td>700000</td>
+            <td>2700000</td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Heredia</td>
+            <td>12</td>
+            <td>5</td>
+            <td>1200000</td>
+            <td>50000</td>
+            <td>1150000</td>
+          </tr>
+        </table>
+        <br></br>
+        <button>Actualizar Finanzas</button>
+      </form>
+    </div>
+  );
 }
--- service_system/src/pages/Rutas.js
+++ service_system/src/pages/Rutas.js
@@ -1,3 +1,33 @@
-export default function Rutas(){
-    return <h1>Rutas</h1>
+export default function Rutas() {
+  return (
+    <div>
+      <h1>Planificación de Rutas</h1>
+      <form>
+        <br></br>
+        <h3>Ruta</h3> <br></br>
+        <select>
+          <option>Ciudad Quesada - Fortuna</option>
+          <option>Ciudad Quesada - Aguas Zarcas</option>
+          <option>Ciudad Quesada - Zarcero</option>
+        </select>
+        <br></br><br></br>
+        <h3>Colaborador</h3>
+        <br></br>
+        <select>
+          <option>Mariana Artavia</option>
+          <option>Francisco Ovares</option>
+          <option>Josué Quesada</option>
+        </select>
+        <br></br><br></br>
+        <h3>Movil</h3>
+        <br></br>
+        <select>
+          <option>Movil #2</option>
+          <option>-</option>
+        </select>
+        <br></br><br></br>
+        <button>Asignar Ruta</button>
+      </form>
+    </div>
+  );
 }
