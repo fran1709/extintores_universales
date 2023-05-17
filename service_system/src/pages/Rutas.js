4e49b6698323092422709af1cb7be8128d5b4bf9
franj <fran1709@estudiantec.cr>
2023-05-17 14:40:04 -0600

    info en rutas por sede funcional

--- service_system/src/pages/Rutas.js
+++ service_system/src/pages/Rutas.js
@@ -1,35 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import DataBase from "./DataBase.json"
+import React from "react";
 
 export default function Rutas() {
-  const [data, setData] = useState([
-    {
-      ruta: "",
-      movil: "",
-      colaborador: "Francisco Ovares",
-    },
-    {
-      ruta: "",
-      movil: "",
-      colaborador: "Mariana Artavia",
-    },
-    {
-      ruta: "",
-      movil: "",
-      colaborador: "Josué Quesada",
-    },
-    {
-      ruta: "",
-      movil: "",
-      colaborador: "Andrik Solano",
-    },
-    {
-      ruta: "",
-      movil: "",
-      colaborador: "Javier Arias",
-    },
-  ]);
+  const userData = JSON.parse(localStorage.getItem("user_Logued"));
+  console.log(userData);
+
+  const empleadoCedula = userData.cedula; // Cédula del empleado que deseas buscar
+
+// Buscar la sede basándose en la cédula del empleado
+let sedeEncontrada = null;
+Object.keys(DataBase).forEach((sede) => {
+  const empleados = DataBase[sede].empleados;
+  const empleadoEncontrado = empleados.find((empleado) => empleado.cedula === empleadoCedula);
+  if (empleadoEncontrado) {
+    sedeEncontrada = sede;
+  }
+});
+
+// Imprimir el resultado
+if (sedeEncontrada) {
+  console.log(`La sede del empleado con cédula ${empleadoCedula} es: ${sedeEncontrada}`);
+} else {
+  console.log(`No se encontró la sede del empleado con cédula ${empleadoCedula}`);
+}
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -51,46 +47,34 @@ export default function Rutas() {
             </tr>
           </thead>
           <tbody>
-            {data.map((item, index) => (
-              <tr key={index}>
-                <td>{item.colaborador}</td>
-                <td>
-                  <select
-                    className="form-select"
-                    value={item.movil}
-                    onChange={(event) => {
-                      const newData = [...data];
-                      newData[index].movil = event.target.value;
-                      setData(newData);
-                    }}
-                  >
-                    <option>Movil #1</option>
-                    <option>Movil #2</option>
-                    <option>Movil #3</option>
-                  </select>
-                </td>
-                <td>
-                  <select
-                    className="form-select"
-                    value={item.ruta}
-                    onChange={(event) => {
-                      const newData = [...data];
-                      newData[index].ruta = event.target.value;
-                      setData(newData);
-                    }}
-                  >
-                    <option value="">Seleccione una ruta</option>
-                    {Object.keys(DataBase).map((ciudad, index) =>
-                      DataBase[ciudad].rutas.map((ruta, index) => (
-                        <option key={index} value={ruta.ruta}>
-                          {ciudad} - {ruta.ruta} ({ruta.clientes} clientes)
+            
+              <React.Fragment key={sedeEncontrada}>
+                {DataBase[sedeEncontrada].empleados.map((nombre, empleadoIndex) => (
+                  <tr key={`${empleadoIndex}`}>
+                    <td>{nombre.nombre}</td>
+                    <td>
+                      <select className="form-select">
+                      {DataBase[sedeEncontrada].moviles.map((numero, placa)=>(
+                        <option key={`${placa}`}>
+                          {numero.numero}
                         </option>
-                      ))
-                    )}
-                  </select>
-                </td>
-              </tr>
-            ))}
+                      ))}
+                      </select>
+                    </td>
+                    <td>
+                      <select className="form-select">
+                        <option value="">Seleccione una ruta</option>
+                        {DataBase[sedeEncontrada].rutas.map((ruta, rutaIndex) => (
+                          <option key={`${empleadoIndex}-${rutaIndex}`} value={ruta.ruta}>
+                            {sedeEncontrada} - {ruta.ruta} ({ruta.clientes} clientes)
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                  </tr>
+                ))}
+              </React.Fragment>
+            
           </tbody>
         </table>
         <br />
@@ -98,7 +82,7 @@ export default function Rutas() {
           Completar Asignación
         </button>
       </form>
-
     </div>
   );
+  
 }
